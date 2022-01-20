c9c691e61f471dca97a7a14585cb7044d6251fe1
Nyanchenkov Artem <gorizont.art@yandex.ru>
2022-01-20 16:15:24 +0300

    HM1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import styles from './index.module.css';
+
+function Message (props) {
+    return (
+        <h1 className={styles.test}> {props.msg}</h1>
+    )
+}
+const Parent = () => {
+    const msg = 'Hello react. This is my first functional component';
+    return (
+        <Message msg={msg}/>
+    )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Parent/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
